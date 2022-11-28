e88a17b4225906cfb5e8ceede69e9d1eb7530e4d
Alexis Davidson <alexisdavidson00@gmail.com>
2022-11-28 17:28:29 +0700

    Install mongodb and link vercel project

---
+++ pages/api/hello_world.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).json({ name: 'John Doe' })
+  } else {
+    const { name, location } = req.body
+    req.send({ status: "User created", name, location })
+  }
+}
