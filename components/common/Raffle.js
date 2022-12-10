26a263ba54727cdb21d1ca303ca69cd2322a88df
Alexis Davidson <alexisdavidson00@gmail.com>
2022-12-10 13:56:26 +0700

    Add second raffle

--- components/common/Raffle.js
+++ components/common/Raffle.js
@@ -1,7 +1,12 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
+import { ethers } from 'ethers'
 
-const Raffle = ({id, name, account, raffle, token, web3Handler, slots, allowance, setAllowance, 
-    lastWinner, totalPayouts, totalBurn, isSlotFilled, requestEndRaffle}) => {
+const fromWei = (num) => parseInt(ethers.utils.formatEther(num))
+const toWei = (num) => ethers.utils.parseEther(num.toString())
+
+const Raffle = ({id, name, account, raffle, token, web3Handler, requestEndRaffle}) => {
+    const raffleRef = useRef()
+    raffleRef.current = raffle;
 
     const enterRaffle = async(slotId) => {
         console.log("Enter Raffle " + id + " for slot " + slotId)
@@ -10,14 +15,17 @@ const Raffle = ({id, name, account, raffle, token, web3Handler, slots, allowance
             await web3Handler();
         }
 
-        if (parseInt(allowance) == 0) {
+        console.log("allowance: " + parseInt(raffle.allowance))
+
+        if (parseInt(raffle.allowance) == 0) {
             console.log("Approve")
-            await token.approve(raffle.address, toWei(10_000_000_000))
-            let all = fromWei(await token.allowance(acc, raffle.address))
-            setAllowance(all)
+            await token.approve(raffle.contractInstance.address, toWei(10_000_000_000))
+            let all = fromWei(await token.allowance(account, raffle.contractInstance.address))
+            raffleRef.current.allowance = all
+            return
         }
 
-        await raffle.play(slotId)
+        await raffle.contractInstance.play(slotId)
     }
 
     const pullOutRaffle = async(slotId) => {
@@ -27,49 +35,57 @@ const Raffle = ({id, name, account, raffle, token, web3Handler, slots, allowance
             await web3Handler();
         }
 
-        await raffle.pullOut(slotId)
+        await raffle.contractInstance.pullOut(slotId)
     }
 
     return (
         <div className="col mx-auto overflow-hidden raffle-column">
-            <div className="row">
-                <div className="card bg-dark mb-2">
-                    <div className="card-header raffle-card-header">{name}</div>
-                </div>
-                <div className="card bg-dark">
-                    <div className="card-body bg-secondary">
-                        <div className="card-text raffle-card-text">
-                            {slots.map((item, idx) => (
-                                item.address != "0x0000000000000000000000000000000000000000" ? (
-                                    <div key={idx} className="raffle-slot raffle-slotTaken" onClick={() => pullOutRaffle(idx)}>
-                                        {item.address.slice(0, 10) + '...' + item.address.slice(32, 42)}
-                                    </div>
-                                ) : (
-                                    <div key={idx} className="raffle-slot raffle-slotFree" onClick={() => enterRaffle(idx)}>
-                                        Click to Enter {name}
-                                    </div>
-                                )
-                            ))}
+            {Object.keys(raffle).length !== 0 ? (
+                <>
+                    <div className="row">
+                        <div className="card bg-dark mb-2">
+                            <div className="card-header raffle-card-header">{name}</div>
+                        </div>
+                        <div className="card bg-dark">
+                            <div className="card-body bg-secondary">
+                                <div className="card-text raffle-card-text">
+                                    {raffle?.slots?.map((item, idx) => (
+                                        item.address != "0x0000000000000000000000000000000000000000" ? (
+                                            <div key={idx} className="raffle-slot raffle-slotTaken" onClick={() => pullOutRaffle(idx)}>
+                                                {item.address.slice(0, 10) + '...' + item.address.slice(32, 42)}
+                                            </div>
+                                        ) : (
+                                            <div key={idx} className="raffle-slot raffle-slotFree" onClick={() => enterRaffle(idx)}>
+                                                Click to Enter {name}
+                                            </div>
+                                        )
+                                    ))}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className="raffle-last-winner row mt-4">
-                <div className="col">Last Winner: {lastWinner}</div>
-            </div>
-            <div className="raffle-last-winner row mt-4">
-                <div className="col">Total Payouts: {totalPayouts}</div>
-            </div>
-            <div className="raffle-last-winner row mt-4">
-                <div className="col">Total Burnt: {totalBurn}</div>
-            </div>
-
-            {isSlotFilled ? (
-                <div className="raffle-slot raffle-slotFree raffle-card-text" onClick={() => requestEndRaffle()}>
-                    Find out who won!
-                </div>
+                    <div className="raffle-last-winner row mt-4">
+                        <div className="col">Last Winner: {raffle.lastWinner}</div>
+                    </div>
+                    <div className="raffle-last-winner row mt-4">
+                        <div className="col">Total Payouts: {raffle.totalPayouts}</div>
+                    </div>
+                    <div className="raffle-last-winner row mt-4">
+                        <div className="col">Total Burnt: {raffle.totalBurn}</div>
+                    </div>
+        
+                    {raffle.isRaffleFilled ? (
+                        <div className="raffle-slot raffle-slotFree raffle-card-text" onClick={() => requestEndRaffle()}>
+                            Find out who won!
+                        </div>
+                    ) : (
+                        <></>
+                    )}
+                </>
             ) : (
-                <></>
+                <div>
+                    Loading...
+                </div>
             )}
         </div>
     );
--- pages/BurnRaffle.js
+++ pages/BurnRaffle.js
@@ -1,71 +1,83 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, forceUpdate } from "react";
 import { Box, Flex, Text } from "@chakra-ui/react";
 import Raffle from "../components/common/Raffle";
 import { ethers } from 'ethers'
 import { motion } from "framer-motion";
 import RaffleAbi from '../lib/contractsData/Raffle.json'
-import RaffleAddress from '../lib/contractsData/Raffle-address.json'
+import RaffleAddress1 from '../lib/contractsData/Raffle-address-1.json'
+import RaffleAddress2 from '../lib/contractsData/Raffle-address-2.json'
 // import TokenAbi from '../lib/contractsData/Token.json'
 // import TokenAddress from '../lib/contractsData/Token-address.json'
 import TokenAbi from '../lib/contractsData/ApeCoinAbi.json'
 import TokenAddress from '../lib/contractsData/ApeCoin-address.json'
 import Axios from 'axios'
 
-const fromWei = (num) => ethers.utils.formatEther(num)
+const fromWei = (num) => parseInt(ethers.utils.formatEther(num))
 const toWei = (num) => ethers.utils.parseEther(num.toString())
 
 function BurnRaffle() {
-	const [raffle, setRaffle] = useState(null);
+	const [raffles] = useState([{}, {}]);
 	const [token, setToken] = useState(null);
     const [account, setAccount] = useState(null)
-    const [slots1, setSlots1] = useState([])
-    const [allowance, setAllowance] = useState("")
-    const [lastWinner, setLastWinner] = useState("")
-    const [totalPayouts, setTotalPayouts] = useState("")
-    const [totalBurn, setTotalBurn] = useState("")
-    const [isSlotFilled, setIsSlotFilled] = useState(false)
-    const raffleRef = useRef();
-    raffleRef.current = raffle;
+    const [firstLoaded] = useState(false)
+    const [forceUpdate, setForceUpdate] = useState(true);
+    const forceUpdateRef = useRef()
+    forceUpdateRef.current = forceUpdate;
+    const firstLoadedRef = useRef(); firstLoadedRef.current = firstLoaded;
+    const rafflesRef = [useRef(), useRef()];
+    rafflesRef[0].current = raffles[0];
+    rafflesRef[1].current = raffles[1];
 
-    const requestEndRaffle = async () => {
+    function useForceUpdate() {
+        setForceUpdate(!forceUpdateRef.current)
+    }
+
+    const requestEndRaffle = async (raffleId) => {
         console.log("requestEndRaffle")
         
-        Axios.post('/api/end_raffle?raffle_address=' + RaffleAddress.address, {
-            raffle_address: RaffleAddress.address
+        Axios.post('/api/end_raffle?raffle_address=' + rafflesRef[raffleId].current.contractInstance.address, {
+            raffle_address: rafflesRef[raffleId].current.contractInstance.address
         }).then((response) => {
             const serverResult = response.data
             console.log(serverResult)
-            loadSlots(raffleRef.current)
+            loadSlots(raffleId)
         })
     }
     
-    const listenToEvents = async (raffle) => {
-        raffle.on("SlotEntered", (user, slot) => {
+    const listenToEvents = async (raffleId) => {
+        if (rafflesRef[raffleId].current.contractInstance == null) {
+            console.log("listenToEvents, raffle null")
+            return
+        }
+        console.log("listenToEvents successfull")
+        rafflesRef[raffleId].current.contractInstance.on("SlotEntered", (user, slot) => {
             console.log("SlotEntered");
             console.log(user, slot);
-            loadSlots(raffleRef.current)
+            loadSlots(raffleId)
         });
-        raffle.on("SlotLeft", (user, slot) => {
+        rafflesRef[raffleId].current.contractInstance.on("SlotLeft", (user, slot) => {
             console.log("SlotLeft");
             console.log(user, slot);
-            loadSlots(raffleRef.current)
+            loadSlots(raffleId)
         });
-        raffle.on("RaffleFilled", () => {
+        rafflesRef[raffleId].current.contractInstance.on("RaffleFilled", () => {
             console.log("RaffleFilled");
-            requestEndRaffle()
+            requestEndRaffle(raffleId)
         });
-        raffle.on("RaffleEnded", () => {
+        rafflesRef[raffleId].current.contractInstance.on("RaffleEnded", () => {
             console.log("RaffleEnded");
-            loadSlots(raffleRef.current)
+            loadSlots(raffleId)
         });
     }
 
-    const loadSlots = async(raffle) => {
-        if (raffle == null)
-            return
-        
+    const loadSlots = async(raffleId) => {
         console.log("Load slots")
-        const participants = await raffle.getParticipants()
+        if (rafflesRef[raffleId]?.current?.contractInstance == null) {
+            console.log("loadSlots null")
+            return
+        }
+        
+        const participants = await rafflesRef[raffleId].current.contractInstance.getParticipants()
         console.log(participants)
 
         let slotsTemp = [
@@ -86,12 +98,15 @@ function BurnRaffle() {
             slotsTemp[i].address = participants[i]
         }
 
-        setSlots1(slotsTemp)
-        
-        setLastWinner(await raffle.lastWinner())
-        setTotalPayouts(fromWei(await raffle.totalPayout()))
-        setTotalBurn(fromWei(await raffle.totalBurned()))
-        setIsSlotFilled((await raffle.participantsCount()) >= 11)
+        console.log(rafflesRef[raffleId].current.contractInstance)
+
+        rafflesRef[raffleId].current.slots = [...slotsTemp]
+        rafflesRef[raffleId].current.lastWinner = await rafflesRef[raffleId].current.contractInstance.lastWinner()
+        rafflesRef[raffleId].current.totalPayouts = fromWei(await rafflesRef[raffleId].current.contractInstance.totalPayout())
+        rafflesRef[raffleId].current.totalBurn = fromWei(await rafflesRef[raffleId].current.contractInstance.totalBurned())
+        rafflesRef[raffleId].current.isRaffleFilled = (await rafflesRef[raffleId].current.contractInstance.participantsCount()) >= 11
+
+        useForceUpdate()
     }
 
     const web3Handler = async () => {
@@ -106,35 +121,50 @@ function BurnRaffle() {
         const provider = new ethers.providers.Web3Provider(window.ethereum)
         const signer = provider.getSigner()
 
-        const raffle = new ethers.Contract(RaffleAddress.address, RaffleAbi.abi, signer)
-        setRaffle(raffle)
+        const raffleObject = {
+            contractInstance: null,
+            allowance: 0,
+            slots: [],
+            isRaffleFilled: false,
+            lastWinner: "",
+            totalPayouts: "",
+            totalBurn: ""
+        }
+
+        const raffles = [raffleObject, raffleObject]
+        raffles[0] = new ethers.Contract(RaffleAddress1.address, RaffleAbi.abi, signer)
+        raffles[1] = new ethers.Contract(RaffleAddress2.address, RaffleAbi.abi, signer)
+
         const token = new ethers.Contract(TokenAddress.address, TokenAbi, signer)
         setToken(token)
-        await loadSlots(raffle)
-        listenToEvents(raffle)
 
-        console.log(acc)
-        console.log(raffle.address)
+        rafflesRef[0].current.contractInstance = raffles[0];
+        rafflesRef[0].current.allowance = fromWei(await token.allowance(acc, raffles[0].address))
 
-        let all = fromWei(await token.allowance(acc, raffle.address))
-        console.log(all)
-        setAllowance(all)
+        rafflesRef[1].current.contractInstance = raffles[1];
+        rafflesRef[1].current.allowance = fromWei(await token.allowance(acc, raffles[1].address))
 
-        if (parseInt(all) == 0) {
-            console.log("Approve")
-            await token.approve(raffle.address, toWei(10_000_000_000))
-            let all = fromWei(await token.allowance(acc, raffle.address))
-            setAllowance(all)
+        for(let i = 0; i < raffles.length; i++) {
+            console.log(raffles[i].address)
+            await loadSlots(i)
+            listenToEvents(i)
         }
+
+        console.log("Acc: " + acc)
     }
-  
+    
     useEffect(() => {
-        web3Handler()
+        if (!firstLoadedRef.current) {
+            web3Handler()
+            firstLoadedRef.current = true
+        }
 
         return () => {
-          raffle?.removeAllListeners("SlotEntered");
-          raffle?.removeAllListeners("RaffleFilled");
-          raffle?.removeAllListeners("SlotLeft");
+            for(let i = 0; i < raffles.length; i++) {
+                raffles[i]?.contractInstance?.removeAllListeners("SlotEntered");
+                raffles[i]?.contractInstance?.removeAllListeners("RaffleFilled");
+                raffles[i]?.contractInstance?.removeAllListeners("SlotLeft");
+            }
         };
     }, [])
 
@@ -145,12 +175,10 @@ function BurnRaffle() {
                     
                     <Flex className="raffle-container" >
                         {/* <div className="raffle-row row"> */}
-                            <Raffle id={0} name={"20M"} account={account} raffle={raffle} web3Handler={web3Handler} 
-                                slots={slots1} allowance={allowance} setAllowance={setAllowance} token={token} 
-                                lastWinner={lastWinner} totalPayouts={totalPayouts} totalBurn={totalBurn} 
-                                isSlotFilled={isSlotFilled} requestEndRaffle={requestEndRaffle} />
-                            {/* <Raffle id={1} name={"50M"} account={account} raffle={raffle} web3Handler={web3Handler} />
-                            <Raffle id={2} name={"200M"} account={account} raffle={raffle} web3Handler={web3Handler} /> */}
+                        <Raffle id={0} name={"20M"} account={account} raffle={raffles[0]} web3Handler={web3Handler} 
+                            token={token} requestEndRaffle={requestEndRaffle} />
+                        <Raffle id={1} name={"50M"} account={account} raffle={raffles[1]} web3Handler={web3Handler} 
+                            token={token} requestEndRaffle={requestEndRaffle} />
                         {/* </div> */}
 
                         {/* <div className="row">
