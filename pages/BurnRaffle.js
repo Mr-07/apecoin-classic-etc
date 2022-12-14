5e4fcfedf0065dfd10bad1013132baf29ec6fee3
Alexis Davidson <alexisdavidson00@gmail.com>
2022-12-14 13:20:04 +0700

    Fix UI for mobile

--- pages/BurnRaffle.js
+++ pages/BurnRaffle.js
@@ -172,25 +172,12 @@ function BurnRaffle() {
         <>
             <Box id="burn-raffle" w="100%" h="100%" bg="#07091b" position="relative">
                 <Flex className="image-wrapper hero-v2" >
-                    
-                    <Flex className="raffle-container" >
-                        {/* <div className="raffle-row row"> */}
+                    <Box className="raffle-container" >
                         <Raffle id={0} name={"20M"} account={account} raffle={raffles[0]} web3Handler={web3Handler} 
                             token={token} requestEndRaffle={requestEndRaffle} />
                         <Raffle id={1} name={"50M"} account={account} raffle={raffles[1]} web3Handler={web3Handler} 
                             token={token} requestEndRaffle={requestEndRaffle} />
-                        {/* </div> */}
-
-                        {/* <div className="row">
-                            <div className="col">Total Payouts: 20,541,654 ACE</div>
-                            <div className="col">Total Burn: 789,123 ACE</div>
-                        </div> */}
-                    </Flex>
-                    {/* <Flex className="about-header-text-container" position={"absolute"} top={["70%", "50%", "40%", "40%", "20%"]} zIndex={12}>
-                        <Text className="about-header-text" fontSize={["24px", "40px", "48px", "56px", "64px"]}>
-                            The Winner of a raffle is determined as soon as all slots are sold out.
-                        </Text>
-                    </Flex> */}
+                    </Box>
                     <motion.div
                         className="box"
                         initial={{ opacity: 1, filter: "blur(0px)" }}
@@ -207,7 +194,6 @@ function BurnRaffle() {
                             srcSet="about-bg-p-500.webp 500w, about-bg-p-800.webp 800w, about-bg-p-1080.webp 1080w, about-bg-p-1600.webp 1600w, about-bg-p-2000.webp 2000w, about-bg-p-2600.webp 2600w, about-bg-p.webp 2880w"
                         />
                     </motion.div>
-                    {/* <div className="position-absolute hero-v2-fade" /> */}
                 </Flex>
             </Box>
         </>
